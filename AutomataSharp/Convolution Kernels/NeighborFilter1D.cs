bd9cbb0d017766334c67c94c4cf1509a5d492077
Stephen Monaco <stevemonaco@hotmail.com>
2020-01-26 20:39:24 -0500

    Simplified Filter into relying on smaller methods

--- AutomataSharp/Convolution Kernels/NeighborFilter1D.cs
+++ AutomataSharp/Convolution Kernels/NeighborFilter1D.cs
@@ -1,22 +1,65 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace AutomataSharp
 {
     public class NeighborFilter1D : IFilterKernel
     {
+        public EdgeWrap EdgeWrap { get; }
         private static IConvolutionRule _emptyRule = new EmptyRule();
-        private static IFilterKernel _defaultFilter = new NeighborFilter1D();
-        private Cell _defaultCell = new Cell(0, _emptyRule, _defaultFilter);
+        private static IFilterKernel _defaultFilter = new NeighborFilter1D(EdgeWrap.Zero);
+        private Cell _defaultCell;
+
+        public NeighborFilter1D(EdgeWrap wrap)
+        {
+            EdgeWrap = wrap;
+
+            if(EdgeWrap == EdgeWrap.Zero)
+                _defaultCell = new Cell(0, _emptyRule, _defaultFilter);
+        }
 
         public IEnumerable<Cell> Filter(Cell[,] grid, int x, int y)
         {
-            var left = x <= 0 ? _defaultCell : grid[x - 1, y];
-            var center = grid[x, y];
-            var right = (x + 1) >= grid.GetLength(0) ? _defaultCell : grid[x + 1, y];
+            if (grid is null)
+                throw new ArgumentNullException($"{nameof(Filter)} parameter {nameof(grid)} is null");
+            if (grid.Length == 0)
+                throw new ArgumentException($"{nameof(Filter)} parameter {nameof(grid)} contained zero cells");
 
-            yield return left;
-            yield return center;
-            yield return right;
+            if (EdgeWrap == EdgeWrap.Zero)
+                return FilterZero(grid, x, y);
+            else if (EdgeWrap == EdgeWrap.Periodic)
+                return FilterPeriodic(grid, x, y);
+            else
+                throw new NotSupportedException($"{nameof(Filter)} {nameof(EdgeWrap)} mode with value ({EdgeWrap}) is not supported");
+        }
+
+        private IEnumerable<Cell> FilterZero(Cell[,] grid, int x, int y)
+        {
+            yield return WrapZero(grid, x - 1, y);
+            yield return WrapZero(grid, x, y);
+            yield return WrapZero(grid, x + 1, y);
+        }
+
+        private Cell WrapZero(Cell[,] grid, int x, int y)
+        {
+            int width = grid.GetLength(0);
+            int height = grid.GetLength(1);
+            if (x < 0 || x >= width || y < 0 || y >= height)
+                return _defaultCell;
+            return grid[x, y];
+        }
+
+        private IEnumerable<Cell> FilterPeriodic(Cell[,] grid, int x, int y)
+        {
+            int length = grid.GetLength(0);
+            yield return grid[WrapIndex(x - 1, length), y];
+            yield return grid[x, y];
+            yield return grid[WrapIndex(x + 1, length), y];
+        }
+
+        private int WrapIndex(int index, int length)
+        {
+            return ((index % length) + length) % length;
         }
     }
 }
